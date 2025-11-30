dd367591adf771ef278ba2f0d9786d411e69a28f
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-11-30 01:17:38 -0500

    solicitud model

--- BACKEND/app/models/solicitudes/solicitud.py
+++ BACKEND/app/models/solicitudes/solicitud.py
@@ -12,3 +12,36 @@ class Solicitud(db.Model):
   fecha_creacion = db.Column(db.DateTime, default=db.func.now())
   
   _estado_str = db.Column('estado', db.String(20), nullable=False, default='BORRADOR')
+  
+  items = db.relationship('ItemSolicitado', backref='solicitud', cascade="all, delete-orphan")
+  
+  def __init__(self, titulo):
+    self.titulo = titulo
+    self._estado_str = "BORRADOR"
+    
+  @property
+  def estado(self):
+    mapeo = {
+      'BORRADOR': Borrador(),
+      'PENDIENTE': Pendiente(),
+      'APROBADA': Aprobada(),
+      'RECHAZADA': Rechazada()
+    }
+    return mapeo.get(self._estado_str, Borrador())
+  
+  def set_estado(self, nuevo_estado_obj):
+    self._estado_str = nuevo_estado_obj.nombre()
+    
+  def agregar_items(self, lista_items: list):
+    self.estado.agregar_items(self, lista_items)
+    
+  def aprobar(self):
+    self.estado.aprobar(self)
+    
+  def rechazar(self):
+    self.estado.rechazar(self)
+    
+  def calcular_total(self):
+    return sum(item.calcular_subtotal() for item in self.items)
+
+  def obtener_tipo_solicitud(self):
